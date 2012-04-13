4083c9e9f6614c8dc282de8951c55d491b8b52ba
Robert Karlsson <RWKarlsson@yahoo.com>
2012-04-13 16:25:15 -0600

    complete session sing in/out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,33 @@
 module SessionsHelper
+	# use Rails cookie utility (hash: value/expires)
+	# current_user is accessible to controllers and views
+	def sign_in(user)
+    cookies.permanent[:remember_token] = user.remember_token
+    current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  # define the assignment w "or equals" (if @current_user is undefined)
+  def current_user
+    @current_user ||= user_from_remember_token
+  end
+
+  def sign_out
+    current_user = nil
+    cookies.delete(:remember_token)
+  end
+
+  private
+
+    def user_from_remember_token
+      remember_token = cookies[:remember_token]
+      User.find_by_remember_token(remember_token) unless remember_token.nil?
+    end
 end
